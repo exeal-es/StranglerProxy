6dae6410a21b53a00180cfe8a7b4e987321d7a67
Damián Pumar <damian.pumar@exeal.com>
2021-09-23 11:02:50 +0200

    Improve controller matcher by httpmethod and route template

--- NetStandardProxy/ReverseProxyMiddleware.cs
+++ NetStandardProxy/ReverseProxyMiddleware.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc.Abstractions;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.AspNetCore.Routing.Template;
 using Microsoft.Extensions.Configuration;
 
 namespace NetStandardProxy
@@ -44,7 +40,7 @@ namespace NetStandardProxy
 
         public Task Invoke(HttpContext context)
         {
-            if (!this.TheRequestHasControllerListening(context))
+            if (!this.HasMatcherController(context))
             {
                 return this.Fordward(context);
             }
@@ -52,13 +48,15 @@ namespace NetStandardProxy
             return this.next.Invoke(context);
         }
 
-        private bool TheRequestHasControllerListening(HttpContext context)
+        private bool HasMatcherController(HttpContext context)
         {
             var path = context.Request.Path.Value;
 
-            var action = this.GetMatchingAction(path, context.Request.Method);
+            var method = context.Request.Method;
 
-            return action != null;
+            var matcher = this.actionDescriptorCollectionProvider.GetPosibleActionMatchersFor(path, method);
+
+            return this.actionSelector.HasMatcherController(matcher);
         }
 
         private async Task Fordward(HttpContext context)
@@ -67,7 +65,7 @@ namespace NetStandardProxy
 
             var uri = new Uri($"{this.destinationURL}{url}");
 
-            var request = CopyRequest(context, uri);
+            var request = context.CloneRequestFor(uri);
 
             var remoteResponse = await this.httpclient.SendAsync(request);
 
@@ -85,88 +83,12 @@ namespace NetStandardProxy
             await remoteResponse.Content.CopyToAsync(actualResponse.Body);
         }
 
-
-        private HttpRequestMessage CopyRequest(HttpContext context, Uri targetUri)
-        {
-            var actualRequest = context.Request;
-
-            var remoteRequest = new HttpRequestMessage()
-            {
-                Method = new HttpMethod(actualRequest.Method),
-                Content = new StreamContent(actualRequest.Body),
-                RequestUri = targetUri,
-            };
-
-            foreach (var header in actualRequest.Headers)
-            {
-                remoteRequest.Content?.Headers.TryAddWithoutValidation(header.Key, header.Value.ToArray());
-            }
-
-            remoteRequest.Headers.Host = targetUri.Host;
-
-            return remoteRequest;
-        }
-
         private void ValidateDestinationURL(string destinationURL)
         {
             if (!Uri.IsWellFormedUriString(destinationURL, UriKind.Absolute))
                 throw new Exception("Please check the destination URL in your appsettings. " +
                     "If you don't added yet, please add this following lines:" +
                     "'ReverseProxy': { 'DestinationURL': 'https://localhost:44322/' }");
-        }
-
-        private ActionDescriptor GetMatchingAction(string path, string httpMethod)
-        {
-            var actionDescriptors = this.actionDescriptorCollectionProvider.ActionDescriptors.Items;
-
-            var matchingDescriptors = new List<ActionDescriptor>();
-
-            foreach (var actionDescriptor in actionDescriptors)
-            {
-                var matchesRouteTemplate = this.MatchesTemplate(actionDescriptor.AttributeRouteInfo.Template, path);
-
-                if (matchesRouteTemplate)
-                {
-                    matchingDescriptors.Add(actionDescriptor);
-                }
-            }
-
-            var httpContext = new DefaultHttpContext();
-
-            httpContext.Request.Path = path;
-
-            httpContext.Request.Method = httpMethod;
-
-            var routeContext = new RouteContext(httpContext);
-
-            return actionSelector.SelectBestCandidate(routeContext, matchingDescriptors.AsReadOnly());
-        }
-
-
-        public bool MatchesTemplate(string routeTemplate, string requestPath)
-        {
-            var template = TemplateParser.Parse(routeTemplate);
-
-            var matcher = new TemplateMatcher(template, this.GetDefaults(template));
-
-            var values = new RouteValueDictionary();
-
-            return matcher.TryMatch(requestPath, values);
-        }
-
-        private RouteValueDictionary GetDefaults(RouteTemplate parsedTemplate)
-        {
-            var result = new RouteValueDictionary();
-
-            foreach (var parameter in parsedTemplate.Parameters)
-            {
-                if (parameter.DefaultValue != null)
-                {
-                    result.Add(parameter.Name, parameter.DefaultValue);
-                }
-            }
-
-            return result;
         }
     }
 }
